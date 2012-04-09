4e0e35b26102a3d5057552b979dbbb6f85b1e0a0
Christian H. Mosveen <neypalm@gmail.com>
2012-04-09 20:42:04 +0200

    Tab fixes

--- public/script.js
+++ public/script.js
@@ -1,22 +1,22 @@
 var showSidebar = function() {
 	$('body').removeClass("active-nav").toggleClass("active-sidebar");
-	$('.menu-button').removeClass("active-button");					
+	$('.menu-button').removeClass("active-button");
 	$('.sidebar-button').toggleClass("active-button");
 }
 
 var showMenu = function() {
 	$('body').removeClass("active-sidebar").toggleClass("active-nav");
-	$('.sidebar-button').removeClass("active-button");				
-	$('.menu-button').toggleClass("active-button");	
+	$('.sidebar-button').removeClass("active-button");
+	$('.menu-button').toggleClass("active-button");
 }
 
 jQuery(document).ready(function($) {
 	$('.menu-button').click(function(e) {
 		e.preventDefault();
-		showMenu();							
+		showMenu();
 	});
 	$('.sidebar-button').click(function(e) {
 		e.preventDefault();
-		showSidebar();									
-	});							
+		showSidebar();
+	});
 });
