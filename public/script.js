6f5252bd516365cdfe5f1f6a744b247185b7875f
Christian H. Mosveen <neypalm@gmail.com>
2012-04-09 20:28:07 +0200

    Off Canvas example

---
+++ public/script.js
@@ -0,0 +1,22 @@
+var showSidebar = function() {
+	$('body').removeClass("active-nav").toggleClass("active-sidebar");
+	$('.menu-button').removeClass("active-button");					
+	$('.sidebar-button').toggleClass("active-button");
+}
+
+var showMenu = function() {
+	$('body').removeClass("active-sidebar").toggleClass("active-nav");
+	$('.sidebar-button').removeClass("active-button");				
+	$('.menu-button').toggleClass("active-button");	
+}
+
+jQuery(document).ready(function($) {
+	$('.menu-button').click(function(e) {
+		e.preventDefault();
+		showMenu();							
+	});
+	$('.sidebar-button').click(function(e) {
+		e.preventDefault();
+		showSidebar();									
+	});							
+});
